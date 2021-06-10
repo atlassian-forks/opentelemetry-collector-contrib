9a591cc2cde6d1b25053218ec5dac7fcffa4482c
Sean ZO Marciniak <smarciniak@atlassian.com>
2021-06-10 13:10:51 +1000

    Adding benchmark

--- exporter/awskinesisexporter/internal/producer/batcher_bench_test.go
+++ exporter/awskinesisexporter/internal/producer/batcher_bench_test.go
@@ -13,3 +13,47 @@
 // limitations under the License.
 
 package producer_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awskinesisexporter/internal/batch"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awskinesisexporter/internal/producer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+)
+
+func benchXEmptyMessages(b *testing.B, msgCount int) {
+	producer, err := producer.NewBatcher(SetPutRecordsOperation(SuccessfulPutRecordOperation), "benchmark-stream",
+		producer.WithLogger(zaptest.NewLogger(b)),
+	)
+
+	require.NoError(b, err, "Must have a valid producer")
+
+	bt := batch.New()
+	for i := 0; i < msgCount; i++ {
+		assert.NoError(b, bt.Add(new(empty.Empty), "fixed-key"))
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		assert.NoError(b, producer.Put(context.Background(), bt))
+	}
+}
+
+func BenchmarkEmptyMessages_X100(b *testing.B) {
+	benchXEmptyMessages(b, 100)
+}
+
+func BenchmarkEmptyMessages_X500(b *testing.B) {
+	benchXEmptyMessages(b, 500)
+}
+
+func BenchmarkEmptyMessages_X1000(b *testing.B) {
+	benchXEmptyMessages(b, 1000)
+}
