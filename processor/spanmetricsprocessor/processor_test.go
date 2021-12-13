040b806a64c33553ac8a59b951aeb841655b4e2d
Claire Fei <cfei@atlassian.com>
2021-12-14 08:15:14 +1100

    parallelise test

--- processor/spanmetricsprocessor/processor_test.go
+++ processor/spanmetricsprocessor/processor_test.go
@@ -700,6 +700,7 @@ func TestValidateDimensions(t *testing.T) {
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 			err := validateDimensions(tc.dimensions, []string{serviceNameKey, spanKindKey, statusCodeKey})
 			if tc.expectedErr != "" {
 				assert.EqualError(t, err, tc.expectedErr)
